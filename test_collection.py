000bf73e22ac588f1abfef9074a2409aec36671c
Rachel <raquel.moreno.carmena@gmail.com>
2019-02-20 23:22:17 +0100

    Collection creation

--- test_collection.py
+++ test_collection.py
@@ -1,9 +1,9 @@
 import pytest
+from collection import Collection
 
 from hypothesis import given, settings, Verbosity
 from hypothesis.strategies import integers, lists
 
-    return new_list
 
 @settings(verbosity=Verbosity.verbose)
 @given(lists(integers()), integers())
@@ -27,11 +27,10 @@ def test_that_a_list_contains_one_more_element_after_insertion(a_list, element):
 #
 #@settings(verbosity=Verbosity.verbose)
 #@given(lists(integers()), lists(integers()))
-#def test_that_two_lists_concatenation_contains_the_elements_of_the_two_list(first_list, second_list):
+#def test_that_two_lists_concatenation_contains_the_elements_of_the_two_list(a_list, another_list):
 #    assert False
 #
 #@settings(verbosity=Verbosity.verbose)
 #@given(lists(integers(min_value=0, max_value=10), min_size=50))
 #def test_that_no_duplicated_elements_when_converting_a_list_to_a_set(a_list):
 #    assert False
-#
