f1507eb1e81344ea475cb355064fb3d838977095
Rachel <raquel.moreno.carmena@gmail.com>
2019-02-20 23:11:00 +0100

    Fibonacci: solved

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -1,6 +1,14 @@
 import pytest
+from fibonacci import fibonacci
 
-#from hypothesis import given, settings, Verbosity
-#from hypothesis.strategies import integers
+from hypothesis import given, settings, Verbosity
+from hypothesis.strategies import integers
 
 
+@settings(verbosity=Verbosity.verbose)
+@given(integers(min_value=0, max_value=100))
+def test_fibonacci(number):
+    assert fibonacci(number) + fibonacci(number + 1) == fibonacci(number + 2)
+
+# An alternative for given: 
+# @given(integers().filter(lambda x: x >= 0 and x <= 100))
