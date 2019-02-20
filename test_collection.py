6ebbab4652250b0eec82e200ced4d6d0fbc85f33
Rachel <raquel.moreno.carmena@gmail.com>
2019-02-20 23:24:13 +0100

    Refactor: rename

--- test_collection.py
+++ test_collection.py
@@ -7,21 +7,21 @@ from hypothesis.strategies import integers, lists
 
 @settings(verbosity=Verbosity.verbose)
 @given(lists(integers()), integers())
-def test_that_a_list_contains_one_more_element_after_insertion(a_list, element):
+def test_that_a_collection_contains_one_more_element_after_insertion(a_list, element):
     collection = Collection(a_list)
 
     assert len(collection.append(element)) == len(collection) + 1
 
 @settings(verbosity=Verbosity.verbose)
 @given(lists(integers()), integers())
-def test_that_a_list_contains_the_inserted_element(a_list, element):
+def test_that_a_collection_contains_the_inserted_element(a_list, element):
     collection = Collection(a_list)
 
     assert element in collection.append(element)
 
 @settings(verbosity=Verbosity.verbose)
 @given(lists(integers(min_value=0, max_value=50)), integers(min_value=0, max_value=50))
-def test_that_a_list_contains_one_element_less_after_deletion(a_list, element):
+def test_that_a_collection_contains_one_element_less_after_deletion(a_list, element):
     collection = Collection(a_list)
     new_collection = collection.append(element)
 
@@ -29,7 +29,7 @@ def test_that_a_list_contains_one_element_less_after_deletion(a_list, element):
 
 @settings(verbosity=Verbosity.verbose)
 @given(lists(integers(min_value=0, max_value=50)), integers(min_value=0, max_value=50))
-def test_that_a_list_does_not_contain_the_removed_element(a_list, element):
+def test_that_a_collection_does_not_contain_the_removed_element(a_list, element):
     collection = Collection(a_list)
     new_collection = collection.append(element)
 
@@ -37,7 +37,7 @@ def test_that_a_list_does_not_contain_the_removed_element(a_list, element):
 
 @settings(verbosity=Verbosity.verbose)
 @given(lists(integers()), lists(integers()))
-def test_that_two_lists_concatenation_contains_the_elements_of_the_two_list(a_list, another_list):
+def test_that_two_collections_concatenation_contains_the_elements_of_the_two_collections(a_list, another_list):
     collection = Collection(a_list)
     another_collection = Collection(another_list)
 
@@ -50,7 +50,7 @@ def test_that_two_lists_concatenation_contains_the_elements_of_the_two_list(a_li
 
 @settings(verbosity=Verbosity.verbose)
 @given(lists(integers(min_value=0, max_value=10), min_size=50))
-def test_that_no_duplicated_elements_when_converting_a_list_to_a_set(a_list):
+def test_that_no_duplicated_elements_when_converting_a_collection_to_a_set(a_list):
     collection = Collection(a_list)
 
     a_set = collection.to_set()
