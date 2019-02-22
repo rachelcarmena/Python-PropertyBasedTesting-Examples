be13615c666b32d570c061bfd0b84ed8e9cc0687
Rachel <raquel.moreno.carmena@gmail.com>
2019-02-22 08:49:07 +0100

    Replacing lists by tuples

--- collection.py
+++ collection.py
@@ -1,35 +1,39 @@
 # Notes:
 # - 'append' and 'remove' methods for a list don't return any value and update the same list
 # - Desire to inmutability
+# - There are tuples as a strategy in Hypothesis, but I didn't find an easy option 
+#   in that library to create a tuple with a size greater than 1 and different elements.
+#   That option is available for lists (min_size).
 class Collection:
 
+
     def __init__(self, a_list):
-        self.a_list = a_list[:]
+        self.a_tuple = tuple(a_list)
 
     def __len__(self):
-        return len(self.a_list)
+        return len(self.a_tuple)
 
     def __iter__(self): 
-        return iter(self.a_list)
+        return iter(self.a_tuple)
 
     def __contains__(self, element):
-        return element in self.a_list
+        return element in self.a_tuple
 
     def to_set(self):
-        a_set = set(self.a_list)
-        return Collection(list(a_set))
+        a_set = set(self.a_tuple)
+        return Collection(tuple(a_set))
 
     def append(self, element):
-        return Collection(self.a_list + [element])
+        return Collection(self.a_tuple + tuple([element]))
 
     def remove(self, element):
-        new_list = self.a_list[:]
+        new_list = list(self.a_tuple)
         new_list.remove(element)
-        return Collection(new_list)
+        return Collection(tuple(new_list))
 
     def concat(self, another_collection):
-        return Collection(self.a_list + another_collection.a_list)
+        return Collection(self.a_tuple + another_collection.a_tuple)
 
     def count(self, element):
-        return self.a_list.count(element)
+        return self.a_tuple.count(element)
 
